68a10103f2567252e87f0f97a5cd317cb2e05f3c
Mohamed Osama Ali <ana20332183@gmail.com>
2024-09-13 16:27:40 +0300

    Add project files.

---
+++ Day01/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day01.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult GetMovie(int id)
+        {
+            return Content($"Movie With Id = {id}");
+        }
+        public IActionResult Index()
+        {
+            //ContentResult result = new ContentResult();
+            //result.Content = "Hello from Index";
+            ////result.ContentType = "text/html"; //default
+            ////result.ContentType = "object/pdf";
+            //return result;
+            return Content("Hello From Index", "text/html");
+        }
+        public IActionResult Test()
+        {
+            //RedirectResult result = new RedirectResult("https://localhost:44353/Movies/index");
+            //return result;
+            //return RedirectToAction(nameof(Index));
+            return RedirectToRoute(new {Controller = "Movies" , action = "Index"});
+
+        }
+    }
+}
